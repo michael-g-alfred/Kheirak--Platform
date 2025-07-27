22d81167cb7c8193f21193916023d61e99d078e5
Michael George Alfred <Michoolgeorge@gmail.com>
2025-07-28 01:27:54 +0300

    Create Contact Form and Update Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PageLayout from "../layouts/PageLayout";
 import Header_Subheader from "../components/Header_Subheader";
+import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
   return (
@@ -9,6 +10,7 @@ export default function Contact() {
         h1="تواصل معنا"
         p="نسعد بتواصلك معنا لأي استفسارات، اقتراحات، أو شراكات محتملة."
       />
+      <ContactForm />
     </PageLayout>
   );
 }
