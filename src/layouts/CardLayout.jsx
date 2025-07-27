e381fa95510dd6a85b50d1fc94ece1ded7efbb60
Michael George Alfred <Michoolgeorge@gmail.com>
2025-07-28 01:16:48 +0300

    Update CardLayout

--- src/layouts/CardLayout.jsx
+++ src/layouts/CardLayout.jsx
@@ -2,21 +2,12 @@ import React from "react";
 
 export default function CardLayout({ title, description, children }) {
   return (
-<<<<<<< Updated upstream
     <div className="rounded-lg p-6 border border-[var(--color-bg-divider)] bg-[var(--color-bg-card)]">
       <h2 className="text-2xl font-semibold mb-2 text-[var(--color-primary-base)] line-clamp-1">
         {title}
       </h2>
       {description && (
         <p className="text-base leading-relaxed text-[var(--color-bg-text)] line-clamp-1">
-=======
-    <div className="rounded-lg shadow p-6 border border-[var(--color-bg-divider)] bg-[var(--color-bg-card)]">
-      <h2 className="text-2xl font-semibold mb-2 text-[var(--color-primary-base)]">
-        {title}
-      </h2>
-      {description && (
-        <p className="text-md leading-relaxed text-[var(--color-bg-text)]">
->>>>>>> Stashed changes
           {description}
         </p>
       )}
