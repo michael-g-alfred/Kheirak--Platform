7eb26dbba47da3392e8be382f18ca0ce8f66c6e4
Michael George Alfred <Michoolgeorge@gmail.com>
2025-08-03 11:00:12 +0300

    Update Navbar font weight from bold to medium
    Changed the font weight of Navbar tab elements from 'font-bold' to 'font-medium' for a more consistent and subtle appearance across different screen sizes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
                       await logout();
                       navigate("/");
                     }}
-                    className="cursor-pointer p-2 text-sm md:text-md lg:text-lg font-bold transition-colors duration-200 rounded-sm danger">
+                    className="cursor-pointer p-2 text-sm md:text-md font-medium transition-colors duration-200 rounded-sm danger">
                     {tab.label}
                   </span>
                 ) : (
@@ -123,7 +123,7 @@ const Navbar = () => {
                     key={tab.id}
                     to={tab.id === "home" ? "/" : `/${tab.id}`}
                     className={({ isActive }) =>
-                      `                    className="cursor-pointer p-1.5 text-sm md:text-md lg:text-lg font-bold transition-colors duration-200 rounded-sm ${
+                      `className="cursor-pointer p-1.5 text-sm md:text-md font-medium transition-colors duration-200 rounded-sm ${
                         tab.id === "login"
                           ? "text-[var(--color-primary-base)] hover:text-[var(--color-bg-text)] hover:bg-[var(--color-primary-hover)]"
                           : isActive
