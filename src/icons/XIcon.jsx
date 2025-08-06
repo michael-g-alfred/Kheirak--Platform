d67dd88124af42cf17f1cce5abe0eeae08d8e066
Michael George Alfred <Michoolgeorge@gmail.com>
2025-08-06 20:29:47 +0300

    Add social and contact SVG icon components
    Introduced new React components for Facebook, Instagram, Mail, Phone, WhatsApp, and X icons in the src/icons directory. These components provide reusable SVG icons for use throughout the application.

---
+++ src/icons/XIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function XIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M4 20L10.768 13.232M13.228 10.772L20 4M4 4L15.733 20H20L8.267 4H4Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
