1a4a73746616e2fbba18b5ad2611aeac25bf3682
Michael George Alfred <Michoolgeorge@gmail.com>
2025-08-12 16:46:17 +0300

    Pagination System
    Created usePagination Hook and Pagination Control component

--- src/components/Admin/CouponReview.jsx
+++ src/components/Admin/CouponReview.jsx
@@ -7,6 +7,8 @@ import { toast } from "react-hot-toast";
 import { useFetchCollection } from "../../hooks/useFetchCollection";
 import { useUpdateStatus } from "../../hooks/useUpdateStatus";
 import { getStatusColor } from "../../utils/statusUtils";
+import { usePagination } from "../../hooks/usePagination";
+import PaginationControls from "../PaginationControls";
 
 export default function CouponReview({ statusFilter = "الكل" }) {
   const { data: coupons, loading, error } = useFetchCollection(["Coupons"]);
@@ -27,93 +29,110 @@ export default function CouponReview({ statusFilter = "الكل" }) {
       ? coupons
       : coupons.filter((coupon) => coupon.status === statusFilter);
 
+  const {
+    currentData: currentCoupons,
+    currentPage,
+    totalPages,
+    nextPage,
+    prevPage,
+  } = usePagination(filteredCoupons, 6);
+
   return (
     <>
       {filteredCoupons.length > 0 ? (
-        <CardsLayout>
-          {filteredCoupons.map((coupon) => (
-            <DynamicCardLayout
-              key={coupon.id}
-              title={coupon.title}
-              status={coupon.status}>
-              <div className="text-md text-[var(--color-bg-text-dark)] space-y-1 text-right">
-                <p>
-                  <strong>المؤسسة: </strong>{" "}
-                  {coupon.submittedBy?.userName || "غير محدد"}
-                </p>
-                <p>
-                  <strong>تفاصيل الكوبون: </strong> {coupon.details}
-                </p>
-                <p>
-                  <strong>الكمية المتوفرة: </strong> {coupon.stock}
-                </p>
-                <p>
-                  <strong>المرفقات: </strong>
-                  {coupon.attachedFiles ? (
-                    Array.isArray(coupon.attachedFiles) ? (
-                      <ul className="list-disc pr-4 space-y-1">
-                        {coupon.attachedFiles.map((file, index) => (
-                          <li key={index}>
-                            <a
-                              href={file}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="text-[var(--color-bg-muted-text)] break-all underline">
-                              {file}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
+        <>
+          <CardsLayout>
+            {currentCoupons.map((coupon) => (
+              <DynamicCardLayout
+                key={coupon.id}
+                title={coupon.title}
+                status={coupon.status}>
+                <div className="text-md text-[var(--color-bg-text-dark)] space-y-1 text-right">
+                  <p>
+                    <strong>المؤسسة: </strong>{" "}
+                    {coupon.submittedBy?.userName || "غير محدد"}
+                  </p>
+                  <p>
+                    <strong>تفاصيل الكوبون: </strong> {coupon.details}
+                  </p>
+                  <p>
+                    <strong>الكمية المتوفرة: </strong> {coupon.stock}
+                  </p>
+                  <p>
+                    <strong>المرفقات: </strong>
+                    {coupon.attachedFiles ? (
+                      Array.isArray(coupon.attachedFiles) ? (
+                        <ul className="list-disc pr-4 space-y-1">
+                          {coupon.attachedFiles.map((file, index) => (
+                            <li key={index}>
+                              <a
+                                href={file}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-[var(--color-bg-muted-text)] break-all underline">
+                                {file}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <a
+                          href={coupon.attachedFiles}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-[var(--color-bg-muted-text)] break-all underline">
+                          {coupon.attachedFiles}
+                        </a>
+                      )
                     ) : (
-                      <a
-                        href={coupon.attachedFiles}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-[var(--color-bg-muted-text)] break-all underline">
-                        {coupon.attachedFiles}
-                      </a>
-                    )
-                  ) : (
-                    "لا يوجد"
-                  )}
-                </p>
-                <p>
-                  <strong>النوع: </strong> {coupon.type}
-                </p>
-                <p className="mt-4 w-full">
-                  <strong>الحالة: </strong>
-                  <span
-                    className={`${getStatusColor(
-                      coupon.status
-                    )} w-full font-bold py-0.125 px-2 rounded`}>
-                    {coupon.status}
-                  </span>
-                </p>
-              </div>
-
-              {coupon.status !== "مكتمل" && (
-                <div className="flex gap-2 mt-4">
-                  <button
-                    onClick={() => updateStatus(coupon.id, "مقبول")}
-                    className="success px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-success-light)] disabled:opacity-50 disabled:cursor-not-allowed"
-                    disabled={
-                      coupon.status === "مقبول" || updatingId === coupon.id
-                    }>
-                    {updatingId === coupon.id ? <Loader /> : "قبول"}
-                  </button>
-                  <button
-                    onClick={() => updateStatus(coupon.id, "مرفوض")}
-                    className="danger px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-danger-light)] disabled:opacity-50 disabled:cursor-not-allowed"
-                    disabled={
-                      coupon.status === "مرفوض" || updatingId === coupon.id
-                    }>
-                    {updatingId === coupon.id ? <Loader /> : "رفض"}
-                  </button>
+                      "لا يوجد"
+                    )}
+                  </p>
+                  <p>
+                    <strong>النوع: </strong> {coupon.type}
+                  </p>
+                  <p className="mt-4 w-full">
+                    <strong>الحالة: </strong>
+                    <span
+                      className={`${getStatusColor(
+                        coupon.status
+                      )} w-full font-bold py-0.125 px-2 rounded`}>
+                      {coupon.status}
+                    </span>
+                  </p>
                 </div>
-              )}
-            </DynamicCardLayout>
-          ))}
-        </CardsLayout>
+
+                {coupon.status !== "مكتمل" && (
+                  <div className="flex gap-2 mt-4">
+                    <button
+                      onClick={() => updateStatus(coupon.id, "مقبول")}
+                      className="success px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-success-light)] disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={
+                        coupon.status === "مقبول" || updatingId === coupon.id
+                      }>
+                      {updatingId === coupon.id ? <Loader /> : "قبول"}
+                    </button>
+                    <button
+                      onClick={() => updateStatus(coupon.id, "مرفوض")}
+                      className="danger px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-danger-light)] disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={
+                        coupon.status === "مرفوض" || updatingId === coupon.id
+                      }>
+                      {updatingId === coupon.id ? <Loader /> : "رفض"}
+                    </button>
+                  </div>
+                )}
+              </DynamicCardLayout>
+            ))}
+          </CardsLayout>
+
+          <PaginationControls
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPrev={prevPage}
+            onNext={nextPage}
+          />
+        </>
       ) : (
         <NoData h2={"لا توجد كوبونات متاحة الآن"} />
       )}
--- src/components/Admin/PostReview.jsx
+++ src/components/Admin/PostReview.jsx
@@ -7,6 +7,8 @@ import { toast } from "react-hot-toast";
 import { useFetchCollection } from "../../hooks/useFetchCollection";
 import { useUpdateStatus } from "../../hooks/useUpdateStatus";
 import { getStatusColor } from "../../utils/statusUtils";
+import { usePagination } from "../../hooks/usePagination";
+import PaginationControls from "../PaginationControls";
 
 export default function PostReview({ statusFilter = "الكل" }) {
   const { data: posts, loading, error } = useFetchCollection(["Posts"]);
@@ -27,90 +29,106 @@ export default function PostReview({ statusFilter = "الكل" }) {
       ? posts
       : posts.filter((post) => post.status === statusFilter);
 
+  const {
+    currentData: currentPosts,
+    currentPage,
+    totalPages,
+    nextPage,
+    prevPage,
+  } = usePagination(filteredPosts, 6);
   return (
     <>
       {filteredPosts.length > 0 ? (
-        <CardsLayout>
-          {filteredPosts.map((post) => (
-            <DynamicCardLayout
-              key={post.id}
-              title={post.title}
-              status={post.status}>
-              <div className="text-md text-[var(--color-bg-text-dark)] space-y-1 text-right">
-                <p>
-                  <strong>مقدم الطلب: </strong>{" "}
-                  {post.submittedBy?.userName || "غير محدد"}
-                </p>
-                <p>
-                  <strong>تفاصيل الطلب: </strong> {post.details}
-                </p>
-                <p>
-                  <strong>المبلغ المطلوب: </strong> {post.amount}
-                </p>
-                <p>
-                  <strong>المرفقات: </strong>
-                  {post.attachedFiles ? (
-                    Array.isArray(post.attachedFiles) ? (
-                      <ul className="list-disc pr-4 space-y-1">
-                        {post.attachedFiles.map((file, index) => (
-                          <li key={index}>
-                            <a
-                              href={file}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="text-[var(--color-bg-muted-text)] break-all underline">
-                              {file}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
+        <>
+          <CardsLayout>
+            {currentPosts.map((post) => (
+              <DynamicCardLayout
+                key={post.id}
+                title={post.title}
+                status={post.status}>
+                <div className="text-md text-[var(--color-bg-text-dark)] space-y-1 text-right">
+                  <p>
+                    <strong>مقدم الطلب: </strong>{" "}
+                    {post.submittedBy?.userName || "غير محدد"}
+                  </p>
+                  <p>
+                    <strong>تفاصيل الطلب: </strong> {post.details}
+                  </p>
+                  <p>
+                    <strong>المبلغ المطلوب: </strong> {post.amount}
+                  </p>
+                  <p>
+                    <strong>المرفقات: </strong>
+                    {post.attachedFiles ? (
+                      Array.isArray(post.attachedFiles) ? (
+                        <ul className="list-disc pr-4 space-y-1">
+                          {post.attachedFiles.map((file, index) => (
+                            <li key={index}>
+                              <a
+                                href={file}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-[var(--color-bg-muted-text)] break-all underline">
+                                {file}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <a
+                          href={post.attachedFiles}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-[var(--color-bg-muted-text)] break-all underline">
+                          {post.attachedFiles}
+                        </a>
+                      )
                     ) : (
-                      <a
-                        href={post.attachedFiles}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-[var(--color-bg-muted-text)] break-all underline">
-                        {post.attachedFiles}
-                      </a>
-                    )
-                  ) : (
-                    "لا يوجد"
-                  )}
-                </p>
-                <p className="mt-4 w-full">
-                  <strong>الحالة: </strong>
-                  <span
-                    className={`${getStatusColor(
-                      post.status
-                    )} w-full font-bold py-0.125 px-2 rounded`}>
-                    {post.status}
-                  </span>
-                </p>
-              </div>
-
-              {post.status !== "مكتمل" && (
-                <div className="flex gap-2 mt-4">
-                  <button
-                    onClick={() => updateStatus(post.id, "مقبول")}
-                    className="success px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-success-light)] disabled:opacity-50 disabled:cursor-not-allowed"
-                    disabled={
-                      post.status === "مقبول" || updatingId === post.id
-                    }>
-                    {updatingId === post.id ? <Loader /> : "قبول"}
-                  </button>
-                  <button
-                    onClick={() => updateStatus(post.id, "مرفوض")}
-                    className="danger px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-danger-light)] disabled:opacity-50 disabled:cursor-not-allowed"
-                    disabled={
-                      post.status === "مرفوض" || updatingId === post.id
-                    }>
-                    {updatingId === post.id ? <Loader /> : "رفض"}
-                  </button>
+                      "لا يوجد"
+                    )}
+                  </p>
+                  <p className="mt-4 w-full">
+                    <strong>الحالة: </strong>
+                    <span
+                      className={`${getStatusColor(
+                        post.status
+                      )} w-full font-bold py-0.125 px-2 rounded`}>
+                      {post.status}
+                    </span>
+                  </p>
                 </div>
-              )}
-            </DynamicCardLayout>
-          ))}
-        </CardsLayout>
+
+                {post.status !== "مكتمل" && (
+                  <div className="flex gap-2 mt-4">
+                    <button
+                      onClick={() => updateStatus(post.id, "مقبول")}
+                      className="success px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-success-light)] disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={
+                        post.status === "مقبول" || updatingId === post.id
+                      }>
+                      {updatingId === post.id ? <Loader /> : "قبول"}
+                    </button>
+                    <button
+                      onClick={() => updateStatus(post.id, "مرفوض")}
+                      className="danger px-6 py-3 rounded text-md focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[var(--color-danger-light)] disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={
+                        post.status === "مرفوض" || updatingId === post.id
+                      }>
+                      {updatingId === post.id ? <Loader /> : "رفض"}
+                    </button>
+                  </div>
+                )}
+              </DynamicCardLayout>
+            ))}
+          </CardsLayout>
+
+          <PaginationControls
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPrev={prevPage}
+            onNext={nextPage}
+          />
+        </>
       ) : (
         <NoData h2={"لا توجد طلبات متاحة الآن"} />
       )}
