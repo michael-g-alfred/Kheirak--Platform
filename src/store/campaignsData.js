55f6085036ac609cf644cbe792956601f2958ac0
khairatalaa4@gmail.com <Menna16920@yahoo.com>
2025-08-13 20:43:58 +0300

    Add campaigns data and implement campaigns UI
    Introduced campaignsData in a new store file and updated Campaigns.jsx to display campaigns, restaurants, and meals. Added functionality for users to select campaigns, view meal details, and confirm purchases, which creates a coupon and notification in Firestore.

---
+++ src/store/campaignsData.js
@@ -0,0 +1,102 @@
+export const campaignsData = [
+  {
+    id: "cmp-food",
+    name: "إطعام",
+    type: "إطعام المحتاجين",
+    restaurants: [
+      {
+        id: "rest1",
+        name: "مطعم باب الحارة",
+        meals: [
+          {
+            id: "meal1",
+            name: "وجبة بيتزا",
+            price: 30,
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Pizza_slice.jpg",
+            description: "بيتزا لذيذة مع الجبن والطماطم الطازجة",
+          },
+          {
+            id: "meal2",
+            name: "فراخ مشوية",
+            price: 50,
+            image: "https://upload.wikimedia.org/wikipedia/commons/4/45/Grilled_chicken.jpg",
+            description: "فراخ مشوية على الفحم مع بهارات خاصة",
+          },
+          {
+            id: "meal3",
+            name: "وجبة دجاج ورز بسمتى",
+            price: 40,
+            image: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Chicken_and_rice.jpg",
+            description: "دجاج متبل مع أرز بسمتي شهي",
+          },
+        ],
+      },
+      {
+        id: "rest2",
+        name: "مطعم أبو صالح",
+        meals: [
+          {
+            id: "meal4",
+            name: "كفتة مشوية",
+            price: 45,
+            image: "https://upload.wikimedia.org/wikipedia/commons/7/70/Grilled_kofte.jpg",
+            description: "كفتة مشوية على الفحم مع سلطة طازجة",
+          },
+          {
+            id: "meal5",
+            name: "سمك مقلي",
+            price: 60,
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/19/Fried_fish.jpg",
+            description: "سمك طازج مقلي ومقدم مع بطاطس مقلية",
+          },
+          {
+            id: "meal6",
+            name: "مكرونة بالصلصة",
+            price: 25,
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Spaghetti_al_pomodoro.JPG",
+            description: "مكرونة اسباجيتي مع صلصة الطماطم",
+          },
+        ],
+      },
+      {
+        id: "rest3",
+        name: "مطعم عزبة البرج",
+        meals: [
+          {
+            id: "meal7",
+            name: "جمبري مشوي",
+            price: 80,
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/13/Grilled_shrimp.jpg",
+            description: "جمبري مشوي مع توابل خاصة",
+          },
+          {
+            id: "meal8",
+            name: "كابوريا بالبخار",
+            price: 90,
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/65/Steamed_crab.jpg",
+            description: "كابوريا طازجة مطهوة بالبخار",
+          },
+          {
+            id: "meal9",
+            name: "أرز صيادية",
+            price: 35,
+            image: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Sayadieh.jpg",
+            description: "أرز صيادية بالسمك الطازج",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "cmp-clothes",
+    name: "كسوة",
+    type: "كسوة الشتاء",
+    restaurants: [],
+  },
+  {
+    id: "cmp-donation",
+    name: "صدقات",
+    type: "تبرعات عامة",
+    restaurants: [],
+  },
+];
