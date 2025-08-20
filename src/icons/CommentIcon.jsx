06535e12dd596103ca87c4ff98ac811d974802f6
Michael George Alfred <Michoolgeorge@gmail.com>
2025-08-20 22:00:21 +0300

    fix: UI and animation
    Improve ChatAi UI and its animation loading state

--- src/icons/CommentIcon.jsx
+++ src/icons/CommentIcon.jsx
@@ -1,8 +1,8 @@
-export default function CommentIcon() {
+export default function CommentIcon({ size = 20 }) {
   return (
     <svg
-      width="20"
-      height="20"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
